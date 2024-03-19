6dcd653285a74d3eadaf7341883ff6c392cca61f
Revvrel <khairul@mkbm.org>
2024-03-19 12:22:22 +0800

    Added Arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,7 @@
+num1 = 10
+num2 = 5
+
+print("Addition: " + str(num1 + num2))
+print("Subtraction: " + str(num1 - num2))
+print("Multiplication: " + str(num1 * num2))
+print("Division: " + str(num1 / num2))
